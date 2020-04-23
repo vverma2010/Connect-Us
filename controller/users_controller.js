3f3f86e1c5623363e1e2c3ff464157e44d4205cb
Vaibhav Verma <56814076+vverma2010@users.noreply.github.com>
2020-04-23 18:17:25 +0530

    Added SCSS, designed Header and Footer,created post

--- controller/users_controller.js
+++ controller/users_controller.js
@@ -64,4 +64,12 @@ module.exports.create = function(req, res){
 module.exports.createSession = function(req, res){
 
     // TODO Later
+    return res.redirect('/');
+}
+
+module.exports.destroySession = function(req,res){
+
+    req.logout();
+
+    return res.redirect('/');
 }
--- index.js
+++ index.js
@@ -4,7 +4,20 @@ const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
+// used for session cookie
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
+const MongoStore = require('connect-mongo')(session);
+const sassMiddleware = require('node-sass-middleware');
+app.use(sassMiddleware({
+    src: './assets/scss',
+    dest: './assets/css',
+    debug: true,
+    outputStyle: 'extended',
+    prefix: '/css'
 
+}));
 app.use(express.urlencoded());
 app.use(cookieParser());
 
@@ -21,7 +34,40 @@ app.set('view engine','ejs');
 app.set('views','./views');
 
 
+
+// Mongo Store is used to store the session cookie in the db
+app.use(session({
+    name:'ConnectUs',
+    //TODO chage the secret before deployment in production mode
+    secret: 'blah-blah',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: (1000 * 60 * 100)
+    },
+    store: new MongoStore(
+        {
+            mongooseConnection: db,
+            autoRemove: 'disabled'
+        },
+        function(err)
+        {
+            console.log(err || 'connect-mongodb setup ok');
+        }
+    )
+
+
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(passport.setAuthenticatedUser);
+
+// use express router
 app.use('/',require('./routes'));
+
+
 
 app.listen(port,function(err){
     if(err)
