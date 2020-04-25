08adea71014095aefa13611d5dfa2450223ecc8b
Vaibhav Verma <56814076+vverma2010@users.noreply.github.com>
2020-04-25 18:30:19 +0530

    All Posts shown in home page along with the user's name

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,11 +1,26 @@
+const Post = require('../model/post');
 module.exports.home = function(req,res)
 {
-    console.log(req.cookies);
-    res.cookie('user_id', 04 );
+    // console.log(req.cookies);
+    // res.cookie('user_id', 04 );
     // return res.end('<h1>express is up for ConnectUs</h1>');
-    return res.render('home' , {
-        title: "Home"
+    Post.find({}, function(req, posts){
+
+        // return res.render('home' , {
+        //     title: "Connect-Us | Home",
+        //     posts : posts
+        // });
     });
+    // populate the user of each post
+    Post.find({}).populate('user').exec(function(err,posts){
+        return res.render('home' , {
+            title: "Connect-Us | Home",
+            posts : posts
+        });
+
+    }) 
+    
+   
 }
 
 //  module.exports.actionName = function(req,res){}
